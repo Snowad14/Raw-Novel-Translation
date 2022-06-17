28f6e62132961e73b4d8e43b9655cf581d67bd4f
Raphael M <snowad142@gmail.com>
2022-06-17 20:41:58 +0200

    dotenv for not leaking my keys

--- translators/keys.py
+++ translators/keys.py
@@ -1,3 +1,8 @@
+# YOU CAN DELETE IF YOU ARE NOT FORKING/DEV THIS PROJECT
+from dotenv import load_dotenv
+import os
+load_dotenv()
+
 # baidu
 APP_ID = '' #你的appid
 SECRET_KEY = '' #你的密钥
@@ -5,4 +10,4 @@ SECRET_KEY = '' #你的密钥
 APP_KEY = '' # 应用ID
 APP_SECRET = '' # 应用秘钥
 # deepl
-DEEPL_AUTH_KEY = 'yourkey' #YOUR_AUTH_KEY
+DEEPL_AUTH_KEY = os.getenv("DEEPLKEY") # CHANGE BY 'YOURKEY' IF YOU ARE NOT FORKING/DEV THIS PROJECT
